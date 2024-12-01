a7c37ac2edf3ba8dbad04a07fdd99e1c88b3f333
SEMIN-97 <jsemin.1021@gmail.com>
2024-12-01 14:52:52 +0900

    feat: Heading tag 변환 로직 구현 (h2 ~ h6)

--- src/rules/headings.ts
+++ src/rules/headings.ts
@@ -1,3 +1,9 @@
 export function convertHeadings(markdown: string) {
-  return markdown.replace(/^# (.*$)/gim, '<h1>$1</h1>');
+  return markdown
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^#### (.*$)/gim, '<h4>$1</h4>')
+    .replace(/^##### (.*$)/gim, '<h5>$1</h5>')
+    .replace(/^###### (.*$)/gim, '<h6>$1</h6>')
 }
